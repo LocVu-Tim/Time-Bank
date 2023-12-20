5b0638962222fab0e3baaf8be9e2d245dd8cca9d
minhtranz <s3978609@rmit.edu.vn>
2023-12-20 15:31:05 +0700

    chore: example header file

---
+++ src/request/controller/RequestController.h
@@ -0,0 +1,15 @@
+#ifndef REQUEST_CONTROLLER_H
+#define REQUEST_CONTROLLER_H
+
+class RequestController {
+public:
+    RequestController();
+    ~RequestController();
+
+    // Add your member functions here
+
+private:
+    // Add your member variables here
+};
+
+#endif // REQUEST_CONTROLLER_H
