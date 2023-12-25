b64b2c9eb70a7243831233b62a97bf8f9ae77f16
zebroe69 <s3927082@rmit.edu.vn>
2023-12-25 22:56:10 +0700

    remove unecessary feature

--- src/user/user.h
+++ src/user/user.h
@@ -1,66 +1,68 @@
 #pragma once
-class User {
-    private:
-    string username, pwd, fullName, email, homeAddr, city, phoneNo;
+class User
+{
+private:
+    string username, pwd, fullName, email, homeAddr, phoneNo;
     bool block;
-    int creds, role;
+    int creds, role, ratingScore;
+
     /*Rating *skillRating, *supporterRating, *hostRating;*/
-    public:
-    //default constructor
+public:
+    // default constructor
     User();
-    //constructor
+    // constructor
     User(string username, string pwd, string fullName, string email,
-    string homeAddr, string city, string phoneNo, int creds);
-    
-    //constructor used for block function
+         string homeAddr, string phoneNo, int creds);
+
+    // constructor used for block function
     User(const string &username);
-    //method to get username
-    string getUsername(); 
-    
-    //method to set username
-    void setUsername(string username); 
+    // method to get username
+    string getUsername();
 
-    //method to get password
-    string getPwd(); 
+    // method to set username
+    void setUsername(string username);
 
-    //method to set password
-    void setPwd(string pwd); 
+    // method to get password
+    string getPwd();
 
-    //method to get city
-    string getCity(); 
+    // method to set password
+    void setPwd(string pwd);
 
-    //method to set city
-    void setCity(string city); 
+    // method to get city
+    string getCity();
 
-    //method to get full name
-    string getFullName(); 
+    // method to set city
+    void setCity(string city);
 
-    //method to set full name 
-    void setFullName(string fullName); 
+    // method to get full name
+    string getFullName();
 
-    //method to get email address
-    string getEmail(); 
+    // method to set full name
+    void setFullName(string fullName);
 
-    //method to set email
-    void setEmail(string email); 
+    // method to get email address
+    string getEmail();
 
-    //method to get home address
-    string getHomeAddr(); 
+    // method to set email
+    void setEmail(string email);
 
-    //method to set home address
-    void setHomeAddr(string homeAddr); 
+    // method to get home address
+    string getHomeAddr();
 
-    //method to get phone number 
+    // method to set home address
+    void setHomeAddr(string homeAddr);
+
+    // method to get phone number
     string getPhoneNo();
 
-    //method to set phone number
-    void setPhoneNo(string phoneNo); 
+    // method to set phone number
+    void setPhoneNo(string phoneNo);
 
-    //method to get credit points
-    int getCreds(); 
+    // method to get credit points
+    int getCreds();
 
-    //method to set credit points
-    void setCreds(int creds); 
+    // method to set credit points
+    void setCreds(int creds);
 
     /*method to get and set role
     1: Guest
@@ -68,64 +70,78 @@ class User {
     3: Admin*/
     int getRole();
 
-    //method to set role
-    void setRole(int role); 
+    // method to set role
+    void setRole(int role);
 
-    //method to get blocked
-    bool isBlocked(); 
+    // method to get blocked
+    bool isBlocked();
 
-    
-    //method to top up credit points with pwd authorization
-    int topUpCreds (User user, int topUp); 
-        
+    // method to top up credit points with pwd authorization
+    //  int topUpCreds (User user, int topUp);
 
-    //method to register to become user (for guest)
-    void registerMember(vector<User *> users); 
-    
+    // method to register to become user (for guest)
+    //  void registerMember(vector<User *> users);
 
-    //method to reset password for member
-    void changePwdMember(User user, string temp); 
+    // method to reset password for member
+    void changePwdMember(User user, string temp);
 
-    //method to change password for admin
-    void changePwdAdmin(vector<User *> users, string username); 
+    // method to change password for admin
+    void changePwdAdmin(vector<User *> users, string username);
 
-    //method to login
-    bool login(vector<User *> users, string checkUsername); 
+    // method to login
+    //  bool login(vector<User *> users, string checkUsername);
 
-    //method to showinfo without rating score 
-    void showInfoWithoutRating(vector<User *> users); 
+    // method to showinfo without rating score
+    void showInfoWithoutRating(vector<User *> users);
 
-    //method to block user from viewing content
-    string blockUser(vector<User *> users, string blocked, string blocker); 
-};  
+    // method to block user from viewing content
+    string blockUser(vector<User *> users, string blocked, string blocker);
 
-//method to compare strings (case insensitive)
-bool caseInsensitiveStringCompare(const string &str1, const string &str2); 
+    // method to compare strings (case insensitive)
+    friend bool caseInsensitiveStringCompare(const string &str1, const string &str2);
 
-//method to compare strings (case sensitive)
-bool caseSensitiveStringCompare(const string &str1, const string &str2); 
+    // method to compare strings (case sensitive)
+    friend bool caseSensitiveStringCompare(const string &str1, const string &str2);
 
-//method to check valid username
-bool checkValidUsername(vector<User *> users, string userName); 
+    // method to check valid username
+    friend bool checkValidUsername(vector<User *> users, string userName);
 
-//method to check valid email
-bool checkValidEmail(vector <User *> users, string email); 
-    
+    // method to check valid email
+    friend bool checkValidEmail(vector<User *> users, string email);
 
-//method to check valid phone number 
-bool checkValidPhoneNo (vector<User *> users, string phoneNo); 
+    // method to check valid phone number
+    friend bool checkValidPhoneNo(vector<User *> users, string phoneNo);
 
-//method to check valid password
-bool checkValidPwd(string pwd); 
+    // method to check valid password
+    friend bool checkValidPwd(string pwd);
 
-//method to verify password
-bool verifyPwd(User user, string pwd); 
+    // method to verify password
+    friend bool verifyPwd(User user, string pwd);
 
-//method to find user thru username
-User* findByUsername(const vector<User*>& users, const string& username); 
+    // method to find user thru username
+    friend User *findByUsername(const vector<User *> &users, const string &username);
+};
 
+// method to compare strings (case insensitive)
+bool caseInsensitiveStringCompare(const string &str1, const string &str2);
 
-int main() {
+// method to compare strings (case sensitive)
+bool caseSensitiveStringCompare(const string &str1, const string &str2);
 
-    return 0;
-}
+// method to check valid username
+bool checkValidUsername(vector<User *> users, string userName);
+
+// method to check valid email
+bool checkValidEmail(vector<User *> users, string email);
+
+// method to check valid phone number
+bool checkValidPhoneNo(vector<User *> users, string phoneNo);
+
+// method to check valid password
+bool checkValidPwd(string pwd);
+
+// method to verify password
+bool verifyPwd(User user, string pwd);
+
+// method to find user thru username
+User *findByUsername(const vector<User *> &users, const string &username);
