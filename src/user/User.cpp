3a7c1e70f2c9f2f4f92b354fbbdf9e8e71a6431f
LAnh-Huy <s3979199@rmit.edu.vn>
2024-01-16 15:48:54 +0700

    refactor: simplify expressions and reformat code

--- src/user/User.cpp
+++ src/user/User.cpp
@@ -8,10 +8,10 @@
 */
 
 User::User() {}
+
 // constructor
 User::User(int userIdVal, string username, string pwd, string fullName, string email,
-           string homeAddr, string phoneNo, int creds, int ratingScore) : role(0)
-{
+           string homeAddr, string phoneNo, int creds, int ratingScore) : role(0) {
     this->userId = userIdVal;
     this->username = username;
     this->pwd = pwd;
@@ -24,91 +24,77 @@ User::User(int userIdVal, string username, string pwd, string fullName, string e
 }
 
 // constructor used for block function
-User::User(const string &username)
-{
+User::User(const string &username) {
     this->username = username;
 }
+
 // method to get username
-string User::getUsername()
-{
+string User::getUsername() {
     return username;
 }
 
 // method to set username
-void User::setUsername(string username)
-{
+void User::setUsername(string username) {
     this->username = username;
 }
 
 // method to get password
-string User::getPwd()
-{
+string User::getPwd() {
     return pwd;
 }
 
 // method to set password
-void User::setPwd(string pwd)
-{
+void User::setPwd(string pwd) {
     this->pwd = pwd;
 }
 
 // method to get full name
-string User::getFullName()
-{
+string User::getFullName() {
     return fullName;
 }
 
 // method to set full name
-void User::setFullName(string fullName)
-{
+void User::setFullName(string fullName) {
     this->fullName = fullName;
 }
 
 // method to get email address
-string User::getEmail()
-{
+string User::getEmail() {
     return email;
 }
 
 // method to set email
-void User::setEmail(string email)
-{
+void User::setEmail(string email) {
     this->email = email;
 }
 
 // method to get home address
-string User::getHomeAddr()
-{
+string User::getHomeAddr() {
     return homeAddr;
 }
 
 // method to set home address
-void User::setHomeAddr(string homeAddr)
-{
+void User::setHomeAddr(string homeAddr) {
     this->homeAddr = homeAddr;
 }
 
 // method to get phone number
-string User::getPhoneNo()
-{
+string User::getPhoneNo() {
     return phoneNo;
 }
 
 // method to set phone number
-void User::setPhoneNo(string phoneNo)
-{
+void User::setPhoneNo(string phoneNo) {
     this->phoneNo = phoneNo;
 }
 
 // method to get credit points
-int User::getCreds()
-{
+int User::getCreds() {
     return creds;
 }
 
 // method to set credit points
-void User::setCreds(int creds)
-{
+void User::setCreds(int creds) {
     this->creds = creds;
 }
 
@@ -116,44 +102,37 @@ void User::setCreds(int creds)
 1: Guest
 2: Member
 3: Admin*/
-int User::getRole()
-{
+int User::getRole() {
     return role;
 }
 
 // method to set role
-void User::setRole(int role)
-{
+void User::setRole(int role) {
     this->role = role;
 }
 
 // method to get blocked
-bool User::isBlocked()
-{
+bool User::isBlocked() {
     return block;
 }
 
 // method to get blocked person/people
-vector<string> User::getBlocked()
-{
+vector<string> User::getBlocked() {
     return blocked;
 }
 
 // method to set block
-void User::setBlocked(string blocks)
-{
-   blocked.push_back(blocks);
+void User::setBlocked(string blocks) {
+    blocked.push_back(blocks);
 }
 
 // method to get userId
-int User::getUserId()
-{
+int User::getUserId() {
     return userId;
 }
 
-void User::setUserId(int userId)
-{
-    this->userId = userId;
+void User::setUserId(int id) {
+    this->userId = id;
 }
 // method to top up credit points with pwd authorization
 //  int User::topUpCreds (User user, int topUp) {
@@ -174,33 +153,26 @@ void User::setUserId(int userId)
 // method to reset password for member
 
 // method to check valid username
-bool checkValidUsername(vector<User *> users, string userName)
-{
+bool checkValidUsername(vector<User *> users, string userName) {
     // check for valid length
-    if (userName.size() < 4)
-    {
+    if (userName.size() < 4) {
         cout << "Username must contain 5 characters or more.\n";
         return false;
     }
 
     // check for no space
-    for (int i = 0; i < userName.length(); i++)
-    {
-        if (isspace(userName[i]))
-        {
+    for (int i = 0; i < userName.length(); i++) {
+        if (isspace(userName[i])) {
             cout << "Username must not contain spaces.\n";
             return false;
         }
     }
 
     // check for matching username
-    for (int i = 0; i < users.size(); i++)
-    {
-        if (caseInsensitiveStringCompare(users[i]->getUsername(), userName) == true)
-        {
+    for (int i = 0; i < users.size(); i++) {
+        if (caseInsensitiveStringCompare(users[i]->getUsername(), userName)) {
             cout << "Username is already taken.\n";
             return false;
-            break;
         }
     }
 
@@ -208,47 +180,37 @@ bool checkValidUsername(vector<User *> users, string userName)
 }
 
 // method to check valid email
-bool checkValidEmail(vector<User *> users, string email)
-{
-    const regex pattern("(\\w+)(\\.|_)?(\\w*)@(\\w+)(\\.(\\w+))+");
+bool checkValidEmail(vector<User *> users, string email) {
+    const regex pattern(R"((\w+)(\.|_)?(\w*)@(\w+)(\.(\w+))+)");
 
     return regex_match(email, pattern);
 }
 
 // method to check valid phone number
-bool checkValidPhoneNo(vector<User *> users, string phoneNo)
-{
+bool checkValidPhoneNo(vector<User *> users, string phoneNo) {
     // phone no must have 10 digits
-    if (phoneNo.length() != 10)
-    {
+    if (phoneNo.length() != 10) {
         cout << "Phone number must have 10 digits.\n";
         return false;
     }
     // phone no must all be digits
-    for (int i = 0; i < phoneNo.length(); i++)
-    {
-        if (!(isdigit(phoneNo[i])))
-        {
+    for (int i = 0; i < phoneNo.length(); i++) {
+        if (!(isdigit(phoneNo[i]))) {
             cout << "Phone number must only contain digits\n";
             return false;
-            break;
         }
     }
     // phone no must start with 0
-    if (!(phoneNo[0] == '0'))
-    {
+    if (phoneNo[0] != '0') {
         cout << "Phone number must start with 0\n";
         return false;
     }
 
     // check if phone no alr existed
-    for (int i = 0; i < users.size(); i++)
-    {
-        if (caseSensitiveStringCompare(users[i]->getPhoneNo(), phoneNo) == true)
-        {
+    for (int i = 0; i < users.size(); i++) {
+        if (caseSensitiveStringCompare(users[i]->getPhoneNo(), phoneNo)) {
             cout << "Phone number already existed.\n";
             return false;
-            break;
         }
     }
 
@@ -256,18 +218,14 @@ bool checkValidPhoneNo(vector<User *> users, string phoneNo)
 }
 
 // method to check valid password
-bool checkValidPwd(string pwd)
-{
-    if (pwd.length() < 8)
-    {
+bool checkValidPwd(string pwd) {
+    if (pwd.length() < 8) {
         cout << "Password must be at least 8 characters long\n";
         return false;
     }
 
-    for (int i = 0; i < pwd.length(); i++)
-    {
-        if (isspace(pwd[i]))
-        {
+    for (int i = 0; i < pwd.length(); i++) {
+        if (isspace(pwd[i])) {
             cout << "Password must not contain space\n";
             return false;
         }
@@ -276,23 +234,18 @@ bool checkValidPwd(string pwd)
 }
 
 // method to verify password
-bool verifyPwd(User user, string pwd)
-{
-    if (caseSensitiveStringCompare(user.getPwd(), pwd) == false)
-    {
-        cout << "Inccorrect password\n";
+bool verifyPwd(User user, string pwd) {
+    if (!caseSensitiveStringCompare(user.getPwd(), pwd)) {
+        cout << "Incorrect password\n";
         return false;
     }
     return true;
 }
 
-// method to find user thru username
-User *findByUsername(const vector<User *> &users, const string &username)
-{
-    for (const auto &userPtr : users)
-    {
-        if (caseInsensitiveStringCompare(userPtr->getUsername(), username))
-        {
+// method to find user through username
+User *findByUsername(const vector<User *> &users, const string &username) {
+    for (const auto &userPtr: users) {
+        if (caseInsensitiveStringCompare(userPtr->getUsername(), username)) {
             return userPtr;
         }
     }
@@ -300,12 +253,9 @@ User *findByUsername(const vector<User *> &users, const string &username)
     return nullptr; // Return nullptr if user is not found
 }
 
-User *findById(const vector<User *> &users, const int targetId)
-{
-    for (const auto &userPtr : users)
-    {
-        if (userPtr->userId == targetId)
-        {
+User *findById(const vector<User *> &users, const int targetId) {
+    for (const auto &userPtr: users) {
+        if (userPtr->userId == targetId) {
             return userPtr;
         }
     }
